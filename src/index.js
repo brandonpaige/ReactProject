1a9b8c6104dab0b2150ba73d6b64ec31d6425a7c
brandonpaige <61633735+brandonpaige@users.noreply.github.com>
2021-07-20 13:26:15 -0400

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+// var React = require("react");
+// var ReactDom = require("react-dom");
+
+import React from "react";
+import ReactDom from "react-dom";
+
+ReactDom.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
